b08479610429afaf7170bc910b8d51f2ce02635e
Zakary <119669322+zakaryjs@users.noreply.github.com>
2024-03-04 18:26:52 +0800

    feat: added functionality to calculate sequences

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    loop {
+
+        println!("Input the number you would like to see the Fibonacci Sequence for:");
+
+        let mut number = String::new();
+
+        io::stdin().read_line(&mut number).expect("Error.");
+
+        let number: u32 = match number.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue
+        };
+
+        for num in 0..number + 1 {
+            println!("fibonacci - {num}: {}", fibonacci_sequence(num));
+        }
+    }
+}
+
+fn fibonacci_sequence(num: u32) -> u32 {
+
+    if num == 0 {
+        return 0
+    }
+    if num == 1 {
+        return 1
+    }
+    if num == 2 {
+        return num
+    }
+    else {
+        return fibonacci_sequence(num - 1) + fibonacci_sequence(num - 2);
+    }
 }
